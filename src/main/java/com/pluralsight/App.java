7e19a7a63c900e987f9cad60aa70940aa138d824
Student <bwilliams1@my.yearupunited.org>
2025-12-01 13:06:00 -0500

    Finished

--- src/main/java/com/pluralsight/App.java
+++ src/main/java/com/pluralsight/App.java
@@ -1,5 +1,28 @@
 package com.pluralsight;
 
-public class App {
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
 
+public class App {
+    public static void main(String[] args) {
+        try {
+            Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/northwind", "root", "Brandin2007$");
+
+            Statement statement = connection.createStatement();
+
+            String query = "SELECT * FROM Products";
+
+            ResultSet results = statement.executeQuery(query);
+
+            while (results.next()) {
+                String name = results.getString("ProductName");
+                System.out.println(name);
+            }
+            connection.close();
+        } catch (Exception e) {
+
+        }
+    }
 }
