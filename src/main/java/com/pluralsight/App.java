78eec0878389984c3960212f16c18a921cfbfb5d
Student <bwilliams1@my.yearupunited.org>
2025-12-02 10:08:48 -0500

    Finally Implemented

--- src/main/java/com/pluralsight/App.java
+++ src/main/java/com/pluralsight/App.java
@@ -1,92 +1,100 @@
 package com.pluralsight;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.ResultSet;
-import java.sql.Statement;
+import java.lang.reflect.Type;
+import java.sql.*;
 import java.util.Scanner;
 
 public class App {
     private static final Scanner keyboard = new Scanner(System.in);
     public static void main(String[] args) {
         try {
-            Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/northwind", "root", "Brandin2007$");
-            System.out.println("What do you want to do?");
-            System.out.println("\t1) Display all products");
-            System.out.println("\t2) Display all customers");
-            System.out.println("\t0) Exit");
-            System.out.print("Enter an option above: ");
-            String option = keyboard.nextLine();
+            String url = args[0];
+            String user = args[1];
+            String password = args[2];
 
-            switch (option) {
-                case "1" -> productsDisplay(connection);
-                case "2" -> customerDisplay(connection);
-                case "0" -> System.exit(0);
-                default -> System.out.println("Something went wrong try again");
+            Connection connection = DriverManager.getConnection(url, user, password);
+            while (true) {
+                System.out.println("What do you want to do?");
+                System.out.println("\t1) Display all products");
+                System.out.println("\t2) Display all customers");
+                System.out.println("\t0) Exit");
+                System.out.print("Enter an option above: ");
+                String option = keyboard.nextLine();
+
+                switch (option) {
+                    case "1" -> displayData(connection, "Products");
+                    case "2" -> displayData(connection, "Customers");
+                    case "0" -> System.exit(0);
+                    default -> System.out.println("Something went wrong try again");
+                }
             }
-        } catch (Exception e) {
-
+        } catch (SQLException e) {
+            System.out.println("Something went wrong");
         }
     }
 
-    public static void productsDisplay(Connection connection) {
+    public static void displayData(Connection connection, String type) {
+        Statement statement = null;
+        ResultSet results = null;
+
         try {
-            Statement statement = connection.createStatement();
+            String query = "SELECT * FROM " + type;
+            statement = connection.prepareStatement(query);
+            results = statement.executeQuery(query);
 
-            String query = "SELECT * FROM Products";
+            if (type.equalsIgnoreCase("Customers")) {
+                while (results.next()) {
+                    String  contactName = results.getString("ContactName");
+                    String companyName = results.getString("CompanyName");
+                    String city = results.getString("City");
+                    String country = results.getString("Country");
+                    String phoneNumber = results.getString("Phone");
 
-            ResultSet results = statement.executeQuery(query);
+                    System.out.println("===============================");
+                    System.out.println("Contact Name: " + contactName);
+                    System.out.println("Company Name: " + companyName);
+                    System.out.println("City: " + city);
+                    System.out.println("Country: " + country);
+                    System.out.println("Phone Number: " + phoneNumber);
+                    System.out.println("===============================");
+                    System.out.println();
+                }
+            } else if (type.equalsIgnoreCase("Products")) {
+                while (results.next()) {
+                    int id = results.getInt("ProductID");
+                    String name = results.getString("ProductName");
+                    double unitPrice = results.getInt("UnitPrice");
+                    int unitsInStock = results.getInt("UnitsInStock");
 
-            while (results.next()) {
-                int id = results.getInt("ProductID");
-                String name = results.getString("ProductName");
-                double unitPrice = results.getInt("UnitPrice");
-                int unitsInStock = results.getInt("UnitsInStock");
-                System.out.println("===============================");
-                System.out.println("Product Id: " + id);
-                System.out.println("Name: " + name);
-                System.out.println("Price: " + unitPrice);
-                System.out.println("Units In Stock: " + unitsInStock);
-                System.out.println("===============================");
-                System.out.println();
+                    System.out.println("===============================");
+                    System.out.println("Product Id: " + id);
+                    System.out.println("Name: " + name);
+                    System.out.println("Price: " + unitPrice);
+                    System.out.println("Units In Stock: " + unitsInStock);
+                    System.out.println("===============================");
+                    System.out.println();
+                }
             }
-            connection.close();
-            statement.close();
             results.close();
-        } catch(Exception e){
-
-        }
-    }
-
-    public static void customerDisplay(Connection connection) {
-        try {
-            Statement statement = connection.createStatement();
-
-            String query = "SELECT * FROM Customers";
-
-            ResultSet results = statement.executeQuery(query);
-
-            while (results.next()) {
-                String  contactName = results.getString("ContactName");
-                String companyName = results.getString("CompanyName");
-                String city = results.getString("City");
-                String country = results.getString("Country");
-                String phoneNumber = results.getString("Phone");
-
-                System.out.println("===============================");
-                System.out.println("Contact Name: " + contactName);
-                System.out.println("Company Name: " + companyName);
-                System.out.println("City: " + city);
-                System.out.println("Country: " + country);
-                System.out.println("Phone Number: " + phoneNumber);
-                System.out.println("===============================");
-                System.out.println();
+            statement.close();
+        } catch(SQLException e){
+            System.out.println("Something has gone wrong in getting all you code");
+        } finally {
+            if (results != null) {
+                try {
+                    results.close();
+                } catch (SQLException e) {
+                    System.out.println("something went wrong when closing your result");
+                }
             }
-            connection.close();
-            statement.close();
-            results.close();
-        } catch(Exception e){
+            if (statement != null) {
+                try {
+                    statement.close();
+                } catch (SQLException e) {
+                    System.out.println("something went wrong when closing your statement");
+                }
 
+            }
         }
     }
 }
